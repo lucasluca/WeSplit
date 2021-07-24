3d6597cc8166ba75cecea480f78600cbc4649c5a
Lucas Mendes <lucas.augustus@gmail.com>
2021-07-24 14:29:14 -0300

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Lucas Mendes on 24/07/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
